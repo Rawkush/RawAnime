2fab07fe027fafad6b0d0a22b80675dc117e68c8
rawkush <ankushrawat950@gmail.com>
2018-07-16 16:23:52 +0530

    recycler adapter

--- app/src/main/java/com/example/ankush/rawanime/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/ankush/rawanime/RecyclerViewAdapter.java
@@ -1,4 +1,35 @@
 package com.example.ankush.rawanime;
 
-public class RecyclerViewAdapter {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends  RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
 }
