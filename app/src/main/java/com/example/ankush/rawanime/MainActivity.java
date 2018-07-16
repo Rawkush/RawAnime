dfd1adcf5c0a043d8198dd17986e03dcfaee6052
rawkush <ankushrawat950@gmail.com>
2018-07-16 15:21:49 +0530

    adding recyclerView

--- app/src/main/java/com/example/ankush/rawanime/MainActivity.java
+++ app/src/main/java/com/example/ankush/rawanime/MainActivity.java
@@ -18,12 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     private final String  mainPageUrl="https://www4.gogoanime.se/";
 
-    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.textView);
         MyAsyncTask task= new MyAsyncTask();
         task.execute();
     }
@@ -33,47 +31,43 @@ public class MainActivity extends AppCompatActivity {
 
     private class MyAsyncTask extends AsyncTask<Void,Void,Void>{
 
+        @Override
+        protected Void doInBackground(Void... voids) {
+            List<AnimeModel> data= new ArrayList<>();
 
-         @Override
-         protected Void doInBackground(Void... voids) {
-             List<AnimeModel> data= new ArrayList<>();
+            try {
+                Document doc = Jsoup.connect(mainPageUrl).get();
+                Elements container= doc.select("div.last_episodes.loaddub"); //
+                Elements container2=container.select("ul.items");
+                Elements dataContainer=container2.select("li");
+                for(Element element:dataContainer){
 
-             if(mainPageUrl!=null){
-                 try {
-                     Document doc = Jsoup.connect(mainPageUrl).get();
-                     Elements container= doc.select("div.last_episodes.loaddub"); //
-                     Elements container2=container.select("ul.items");
-                     Elements dataContainer=container2.select("li");
-                     for(Element element:dataContainer){
-
-                        Elements Episode=element.select("p.episode");
-                        String episode= Episode.text();
-
-                        Elements title=element.select("p.name");
-                        String tile=title.text();
-                        Log.d("title",tile);
-
-                        Elements img=element.select("div.img");
-                        Element links = img.select("a[href]").first(); // a with href
-                        String nextPageLink=links.attr("href");
-                        nextPageLink=mainPageUrl+nextPageLink;
-                        Element ImageLink= links.select("img").first();
-                        String  imgLink=ImageLink.attr("src");
-                        Log.d("links",imgLink);
-
-                     }
+                    Elements Episode=element.select("p.episode");
+                    String episode= Episode.text();
+                    Elements titles=element.select("p.name");
+                    String title=titles.text();
+                    Log.d("title",title);
+                    Elements img=element.select("div.img");
+                    Element links = img.select("a[href]").first(); // a with href
+                    String nextPageLink=links.attr("href");
+                    nextPageLink=mainPageUrl+nextPageLink;
+                    Element ImageLink= links.select("img").first();
+                    String  imgLink=ImageLink.attr("src");
+                    Log.d("links",imgLink);
+                    data.add(new AnimeModel(episode,imgLink,title,nextPageLink));
+                }
 
 
-                 } catch (IOException e) {
-                     e.printStackTrace();
-                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
 
-             }
 
-         return null;
 
-         }
+            return null;
+
+        }
 
     }
 
